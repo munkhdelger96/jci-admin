1a97f92853bf07843e61b1ff323da49207239370
munkhdelger96 <munkhdelger@y7mail.com>
2020-02-09 08:50:06 +0900

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -28,17 +28,7 @@ class Footer extends React.Component {
     return (
       <footer className="footer">
         <Container fluid>
-          <Nav>
-            <NavItem>
-              <NavLink href="javascript:void(0)">Creative Tim</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="javascript:void(0)">About Us</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="javascript:void(0)">Blog</NavLink>
-            </NavItem>
-          </Nav>
+          
           <div className="copyright">
             © {new Date().getFullYear()} made with{" "}
             <i className="tim-icons icon-heart-2" /> by{" "}
