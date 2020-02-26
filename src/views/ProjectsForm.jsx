92d412b16dbab6086fb3dcb9f2d49569836fbcfe
ErkaChopper <60495703+ErkaChopper@users.noreply.github.com>
2020-02-27 00:32:43 +0800

    erka
    asdsad

---
+++ src/views/ProjectsForm.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import axios from "axios";
+
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col
+} from "reactstrap";
+
+class ProjectsForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      position: '',
+      joinedDate: '',
+      image: '',
+      rank: ''
+    };
+  }
+
+  onSubmit = (e) => {
+    console.error('asd');
+    e.preventDefault();
+
+    const { title, description, author } = this.state;
+
+    axios.post('/members')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        console.log('here');
+        // always executed
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col md="8">
+              <Card>
+                <CardHeader>
+                  <h5 className="title">Add member</h5>
+                </CardHeader>
+                <CardBody>
+                  <Form onSubmit={this.onSubmit}>
+                    <Row>
+                      <Col className="pr-md-1" md="5">
+                        <FormGroup>
+                        <label>First name </label>
+                          <Input
+                            placeholder="Narmandakh"
+                            type="text"
+                            required="true"
+                          />
+                        </FormGroup>
+                      </Col>
+                      </Row>
+                      <Row>
+                      <Col className="pr-md-1" md="5">
+                        <FormGroup>
+                          <label>Last name</label>
+                          <Input
+                            defaultValue=""
+                            placeholder="Lkhagvajav"
+                            type="text"
+                            required="true"
+                          />
+                        </FormGroup>
+                      </Col>
+                      </Row>
+                      <Row>
+                        <Col className="pr-md-1" md="5">
+                          <FormGroup>
+                            <label>Position</label>
+                            <Input placeholder="Director" 
+                                    type="text"
+                                    required="true"
+                                    />
+                          </FormGroup>
+                         </Col>
+                      </Row>
+                      <Row>
+                        <Col className="pr-md-1" md="5">
+                          <FormGroup>
+                            <label>Rank</label>
+                            <Input placeholder="1" type="number" />
+                          </FormGroup>
+                         </Col>
+                      </Row>  
+                      <Row>
+                        <Col className="pr-md-1" md="5">
+                          <FormGroup>
+                            <label>Image</label>
+                            <Input placeholder="" type="text" />
+                          </FormGroup>
+                         </Col>
+                      </Row>
+                      <Row>
+                        <Col className="pr-md-1" md="5">
+                          <FormGroup>
+                            <label>Joined Date</label>
+                            <Input type="Date" />
+                          </FormGroup>
+                         </Col>
+                      </Row>  
+                      <Button className="btn-fill" color="primary" type="submit">
+                        Бүртгэх
+                      </Button>     
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ProjectsForm;
